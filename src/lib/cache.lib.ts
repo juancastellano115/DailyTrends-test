06ef1765c04ce524a8371b8dcef68337aa3ad45b
Juan Castellano <juancastellano115@gmail.com>
2023-10-30 18:00:34 +0100

    Feat: Add cache lib

---
+++ src/lib/cache.lib.ts
@@ -0,0 +1,40 @@
+import type { RedisClientType } from 'redis';
+import { createClient } from 'redis';
+import { logger } from '../utils/logger';
+
+let redisClient: RedisClientType;
+let isReady: boolean;
+
+const cacheOptions = {
+  url: process.env.REDIS_URL,
+};
+
+export const REDIS_EXPIRATION_TIME = 60;
+
+async function getCache(): Promise<RedisClientType> {
+  if (!isReady) {
+    redisClient = createClient({
+      ...cacheOptions,
+    });
+    redisClient.on('error', err => logger.error(`Redis Error: ${err}`));
+    redisClient.on('connect', () => logger.info('Redis connected'));
+    redisClient.on('reconnecting', () => logger.info('Redis reconnecting'));
+    redisClient.on('ready', () => {
+      isReady = true;
+      logger.info('Redis ready!');
+    });
+    await redisClient.connect();
+  }
+  return redisClient;
+}
+
+getCache()
+  .then(connection => {
+    redisClient = connection;
+  })
+  .catch(err => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    logger.error(`Error connecting to Redis: ${err}`);
+  });
+
+export { getCache };
